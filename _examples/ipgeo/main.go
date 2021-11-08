71260c607052ffe227685e315c8c71c64d9cc010
BRUHItsABunny <bruhitsabunny@gmail.com>
2021-11-08 04:30:50 +0100

    post-test, success

--- _examples/ipgeo/main.go
+++ _examples/ipgeo/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/BRUHItsABunny/bigdatacloud-apis/client"
+	"github.com/davecgh/go-spew/spew"
+)
 
+func main() {
+	userAgent := ""
+	key := ""
+
+	c := client.NewBigDataCloudClient(nil, key, userAgent)
+	res, err := c.IPGeolocationFull(context.Background(), "", "178.18.25.2")
+	if err == nil {
+		fmt.Println(spew.Sdump(res))
+	} else {
+		fmt.Println("err: ", err)
+	}
 }
--- _examples/reversegeo/main.go
+++ _examples/reversegeo/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/BRUHItsABunny/bigdatacloud-apis/client"
+	"github.com/davecgh/go-spew/spew"
+)
 
+func main() {
+	userAgent := ""
+	key := ""
+
+	c := client.NewBigDataCloudClient(nil, key, userAgent)
+	res, err := c.ReverseGeocodeWithTimezoneRequest(context.Background(), "", 52.370216, 4.895168)
+	if err == nil {
+		fmt.Println(spew.Sdump(res))
+	} else {
+		fmt.Println("err: ", err)
+	}
 }
